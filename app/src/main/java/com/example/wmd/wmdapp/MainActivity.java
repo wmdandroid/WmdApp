44bbf4763bb53ba3a49d6c1ec6cda50b8258b2fc
wmdmgv <gleb.metlov@rebilly.com>
2017-01-17 13:33:33 +0300

    remove unused code

--- app/src/main/java/com/example/wmd/wmdapp/MainActivity.java
+++ app/src/main/java/com/example/wmd/wmdapp/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.wmd.wmdapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
